6e73825afb235d92bfb33832001771a13206c3a5
Alay Patel <alaypatel212@gmail.com>
2026-01-24 23:02:23 +0530

    Added test to CI Pipeline

--- backend/test/test_api.py
+++ backend/test/test_api.py
@@ -1,27 +1,19 @@
-# from fastapi.testclient import TestClient
-# import sys
-# import os
+from fastapi.testclient import TestClient
+from main import app
 
-# sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-# from main import app
+client = TestClient(app)
 
-# client = TestClient(app)
+def test_health_returns_200():
+    response = client.get("/health")
+    assert response.status_code == 200
+    
+def test_status():
+    payload = {"status": "UP", "response_time_ms": 12.5}
 
+    res = client.post("/status", json=payload)
+    assert res.status_code == 200
 
-# def test_health():
-#     res = client.get("/health")
-#     assert res.status_code == 200
-#     data = res.json()
-#     assert data["status"] == "UP"
-
-
-# def test_status():
-#     payload = {"status": "UP", "response_time_ms": 12.5}
-
-#     res = client.post("/status", json=payload)
-#     assert res.status_code == 200
-
-#     data = res.json()
-#     assert "logs" in data
-#     assert isinstance(data["logs"], list)
-#     assert len(data["logs"]) <= 10
+    data = res.json()
+    assert "logs" in data
+    assert isinstance(data["logs"], list)
+    assert len(data["logs"]) <= 10
